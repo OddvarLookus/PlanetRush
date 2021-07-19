89631526b992c1d18fea4b2e6039131de5da2643
OddvarLookus <lorenzo1viglietti@gmail.com>
2021-07-19 20:21:22 +0100

    fixed null bug in notification script

--- Data/Hazards/hazard_notification.gd
+++ Data/Hazards/hazard_notification.gd
@@ -11,6 +11,8 @@ onready var viewport_size : Vector2 = get_viewport_rect().size
 
 var can_vanish : bool = true
 
+var last_pos : Vector2
+
 func _ready() -> void:
 	
 	pass
@@ -26,7 +28,11 @@ func set_hazard_reference(_hazard : Node2D, _camera : Camera2D):
 
 func clamp_notification_position():
 	
-	var hz_pos : Vector2 = hazard.global_position
+	var hz_pos : Vector2
+	if(is_instance_valid(hazard)):
+		hz_pos = hazard.global_position
+		last_pos = hz_pos
+	
 	
 	if(is_in_viewport() and can_vanish):
 		anim_player.play("hazard_notification_fade_anim")
@@ -35,8 +41,8 @@ func clamp_notification_position():
 	elif(not is_in_viewport()):
 		var cam_pos = camera.get_camera_position()
 		
-		var pos_x : float = clamp(hz_pos.x, cam_pos.x - viewport_size.x / 2.0 + margin, cam_pos.x + viewport_size.x / 2.0 - margin)
-		var pos_y : float = clamp(hz_pos.y, cam_pos.y + viewport_size.y / 2.0 + margin, cam_pos.x - viewport_size.y / 2.0 - margin)
+		var pos_x : float = clamp(last_pos.x, cam_pos.x - viewport_size.x / 2.0 + margin, cam_pos.x + viewport_size.x / 2.0 - margin)
+		var pos_y : float = clamp(last_pos.y, cam_pos.y + viewport_size.y / 2.0 + margin, cam_pos.x - viewport_size.y / 2.0 - margin)
 		
 		global_position = Vector2(pos_x, pos_y)
 		
@@ -46,17 +52,23 @@ func clamp_notification_position():
 
 func is_in_viewport() -> bool:
 	
-	var cam_pos : Vector2 = camera.get_camera_position()
-	var is_in_x : bool = false
-	var is_in_y : bool = false
+	if(is_instance_valid(hazard)):
+		
+		var cam_pos : Vector2 = camera.get_camera_position()
+		var is_in_x : bool = false
+		var is_in_y : bool = false
+		
+		if(hazard.global_position.x > cam_pos.x - viewport_size.x / 2.0 and hazard.global_position.x < cam_pos.x + viewport_size.x / 2.0):
+			is_in_x = true
+		
+		if(hazard.global_position.y < cam_pos.y + viewport_size.y / 2.0 and hazard.global_position.y > cam_pos.y - viewport_size.y / 2.0):
+			is_in_y = true
+		
+		return (is_in_x and is_in_y)
+	else:
+		return false
+		pass
 	
-	if(hazard.global_position.x > cam_pos.x - viewport_size.x / 2.0 and hazard.global_position.x < cam_pos.x + viewport_size.x / 2.0):
-		is_in_x = true
-	
-	if(hazard.global_position.y < cam_pos.y + viewport_size.y / 2.0 and hazard.global_position.y > cam_pos.y - viewport_size.y / 2.0):
-		is_in_y = true
-	
-	return (is_in_x and is_in_y)
 	pass
 
 func _on_AnimationPlayer_animation_finished(anim_name: String) -> void:
